a36a766d14a4f01d7c45a148fe578ca59a9b83a8
Pete Ngai <sendccme55@gmail.com>
2020-06-03 19:42:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,13 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+
+  current_board_player = board[index]
+  if current_board_player == " " || current_board_player == ""
+    true
+  elsif current_board_player == "X"
+    true
+  elsif current_board_player == "O"
+    true
+  else false
+
+end
